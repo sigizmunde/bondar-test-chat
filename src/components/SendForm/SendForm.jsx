abc8597402a4b362a8d8338104acec066abccc77
illya bondar <1816579@gmail.com>
2022-08-24 01:46:03 +0300

    clearing filter on sending message added

--- src/components/SendForm/SendForm.jsx
+++ src/components/SendForm/SendForm.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { addMessage } from "../../redux/messagesSlice";
 import { Form, SendInput } from "./SendForm.styled";
 import icons from "../../image/icons.svg";
+import { FilterContext } from "../Layout/Layout";
 
 const SendForm = () => {
   const { id } = useParams();
   const [messageText, setMessageText] = useState("");
   const dispatch = useDispatch();
+  const { setFilter } = useContext(FilterContext);
 
   const handleChange = (e) => {
     setMessageText(e.target.value);
@@ -29,6 +31,7 @@ const SendForm = () => {
     e.preventDefault();
     sendMessage(messageText.trim(), id);
     clearForm();
+    setFilter("");
   };
 
   return (
