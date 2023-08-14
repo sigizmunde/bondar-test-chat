114f7f282b45d009a26f13e3922d2316d9dc6a44
developer <1816579@gmail.com>
2023-08-15 01:09:12 +0300

    upd: console log removed

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -12,8 +12,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-console.log(firebaseConfig);
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
